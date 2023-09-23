37011b660be3b9b22a5be4bf02bae474997c6fe6
ss611611 <ss611611@gmail.com>
2023-09-23 13:50:37 +0800

    Variables

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -1,3 +1,54 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let myName = "Vandad"
+let yourName = "Foo"
+
+var names = [
+myName,
+yourName
+]
+
+names.append("Bae")
+names.append("Baz")
+
+let foo = "Foo"
+var foo2 = foo
+foo2 = "Foo 2"
+foo
+foo2
+
+let moreNames = [
+"Foo",
+"Bar"
+]
+var copy = moreNames
+copy.append("Baz")
+moreNames
+copy
+
+let oldArray = NSMutableArray(
+    array: [
+    "Foo",
+    "Bar"
+    ]
+)
+oldArray.add("Baz")
+var newArray = oldArray
+newArray.add("Qux")
+oldArray
+newArray
+
+
+
+let someNames = NSMutableArray(
+    array: [
+    "Foo",
+    "Bar"
+    ]
+)
+func changeTheArray(_ array: NSArray) {
+    let copy = array as! NSMutableArray
+    copy.add("Baz")
+}
+changeTheArray(someNames)
+someNames
