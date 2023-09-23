ef7d831b7923a746a0c8f5e179b5288d1fbec0b4
ss611611 <ss611611@gmail.com>
2023-09-23 16:22:16 +0800

    Functions

---
+++ Functions.playground/Contents.swift
@@ -0,0 +1,79 @@
+import Foundation
+
+func noArgumentsAndReturnValue() {
+    "I don't know what I'm doing"
+}
+
+noArgumentsAndReturnValue()
+
+func plusTwo(value: Int) {
+    let newValue = value + 2
+}
+
+plusTwo(value: 30)
+
+func newPlusTwo(value: Int) -> Int {
+    value + 2
+}
+newPlusTwo(value: 30)
+
+func customAdd(
+    value1: Int,
+    value2: Int
+) -> Int {
+    value1 + value2
+}
+let customAdded = customAdd(
+    value1: 10,
+    value2: 20
+)
+
+
+
+func customMinus(
+   _ lhs: Int,
+   _ rhs: Int
+) -> Int {
+    lhs - rhs
+}
+
+let customSubtracted = customMinus(
+    20,
+    10
+)
+
+customAdd(value1: 20, value2: 30)
+
+
+@discardableResult
+func myCustomAdd(
+    _ lhs: Int,
+    _ rhs: Int
+) -> Int {
+    lhs + rhs
+}
+
+myCustomAdd(20, 30)
+
+
+func doSomethingComplicated(
+    with value: Int
+) -> Int {
+    func mainLogic(value: Int) -> Int {
+        value + 2
+    }
+    return mainLogic(value: value + 3)
+}
+doSomethingComplicated(with: 30)
+
+
+func getFullName(
+    firstName: String = "Foo",
+    lastName: String = "Bar"
+) -> String {
+    "\(firstName) \(lastName)"
+}
+getFullName()
+getFullName(firstName: "Baz")
+getFullName(lastName: "Foo")
+getFullName(firstName:"Baz", lastName: "Qux")
